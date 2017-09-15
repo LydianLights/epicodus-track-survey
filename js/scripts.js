9f5f1aa056c401a67eda7c70cd4fd0071273950f
LydianLights <rane.fields@gmail.com>
2017-09-15 11:25:26 -0700

    Make quiz questions display sequentially as they are answered

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+var goToNextPage = function(currentPage, nextPage) {
+  currentPage.slideUp();
+  nextPage.slideDown();
+}
+
+$(document).ready(function() {
+  // Quiz start button
+  $("#btn-quiz-start").click(function() {
+    goToNextPage($("#intro"), $("#quiz-intro"));
+  });
+
+  // Quiz-intro button
+  $("#quiz-intro form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-intro"), $("#quiz-q1"));
+  });
+
+  // Quiz-q1 button
+  $("#quiz-q1 form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-q1"), $("#quiz-q2"));
+  });
+
+  // Quiz-q2 button
+  $("#quiz-q2 form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-q2"), $("#quiz-q3"));
+  });
+
+  // Quiz-q3 button
+  $("#quiz-q3 form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-q3"), $("#quiz-q4"));
+  });
+
+  // Quiz-q4 button
+  $("#quiz-q4 form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-q4"), $("#quiz-q5"));
+  });
+
+  // Quiz-q5 button
+  $("#quiz-q5 form").submit(function(event) {
+    event.preventDefault();
+    goToNextPage($("#quiz-q5"), $("#result"));
+  });
+});
